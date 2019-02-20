8106be4529d3f45fdd76cfde4e3e660f076088a1
cl-macmini-110 <cl-macmini-110@admins-Mac-mini-2.local>
2019-02-20 14:32:49 +0530

    Initial Commit

---
+++ SignUpValidation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SignUpValidation
+//
+//  Created by cl-macmini-110 on 20/02/19.
+//  Copyright © 2019 cl-macmini-110. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
