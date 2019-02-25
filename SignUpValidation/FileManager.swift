3a250e7efdcdb2ab9514083997595dff92a8b9f2
cl-macmini-110 <cl-macmini-110@admins-Mac-mini-2.local>
2019-02-25 16:21:21 +0530

    first commit

---
+++ SignUpValidation/FileManager.swift
@@ -0,0 +1,9 @@
+//
+//  FileManager.swift
+//  SignUpValidation
+//
+//  Created by cl-macmini-110 on 21/02/19.
+//  Copyright © 2019 cl-macmini-110. All rights reserved.
+//
+
+import Foundation
---
+++ SignUpValidation/RegisterVc.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterVc.swift
+//  SignUpValidation
+//
+//  Created by cl-macmini-110 on 21/02/19.
+//  Copyright © 2019 cl-macmini-110. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
